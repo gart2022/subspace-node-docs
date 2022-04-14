dfa20e5fa507825d0b9c592631c4c53202fc785c
gart2022 <103565754+gart2022@users.noreply.github.com>
2022-04-15 00:41:51 +0800

    Update farmer.sh

--- farmer.sh
+++ farmer.sh
@@ -6,4 +6,4 @@
 # 将WALLET_ADDRESS修改为自己的钱包地址
 # 例子：nohup ./farmer farm --reward-address st8N65fpjgVguZr7kxVk4FMcZphZF465uzDThGhz24BPyQy4v > farmer.log 2>&1 &
 
-nohup ./farmer farm --reward-address WALLET_ADDRESS > farmer.log 2>&1 &
+nohup ./farmer farm --reward-address WALLET_ADDRESS st7MumwAF42jua9HvTkGj3SVC9RHdG5E5N1cbQtSwdSPyWwgi > farmer.log 2>&1 &
